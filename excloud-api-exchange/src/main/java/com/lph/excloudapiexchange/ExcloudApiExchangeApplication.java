2979567cde2e3c4204c98f9591784634c0500788
lupeihe <lupeihe@fang.com>
2018-08-06 18:56:16 +0800

    init

--- excloud-api-exchange/src/main/java/com/lph/excloudapiexchange/ExcloudApiExchangeApplication.java
+++ excloud-api-exchange/src/main/java/com/lph/excloudapiexchange/ExcloudApiExchangeApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableCaching
+//@EnableDiscoveryClient
 public class ExcloudApiExchangeApplication {
 
 	public static void main(String[] args) {
--- excloud-api-exchange/src/test/java/com/lph/excloudapiexchange/ExcloudApiExchangeApplicationTests.java
+++ excloud-api-exchange/src/test/java/com/lph/excloudapiexchange/ExcloudApiExchangeApplicationTests.java
@@ -1,7 +1,5 @@
 package com.lph.excloudapiexchange;
 
-import com.lph.excloudapiexchange.entity.User;
-import com.lph.excloudapiexchange.service.IUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ExcloudApiExchangeApplicationTests {
 
-	@Autowired
-	IUserService userService;
 
 	@Test
 	public void contextLoads() {
